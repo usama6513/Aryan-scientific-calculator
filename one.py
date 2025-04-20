3c326a4bab8dd4677d63bfce3fed44a42e8c54b2
usama6513 <usamasharifk54@gmail.com>
2025-04-21 00:32:18 +0500

    one.py

--- one.py
+++ one.py
@@ -46,63 +46,7 @@ import streamlit as st
 import numpy as np
 import math
 from sympy import symbols, diff, integrate, sympify
-
-
-
-# Custom CSS for enhanced UI (UI ko behtr bnane k lie custom styling)
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #1e1e2f;
-        color: white;
-    }
-    .stApp {
-        background: linear-gradient(135deg, #bcbcbc, #cfe2f3);
-        padding: 30px;
-        border-radius: 15px; 
-        box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.3);
-    }
-    h1 {
-        text-align: center;
-        font-size: 36px;
-        color: white;
-    }
-    .stButton>button {
-        background: linear-gradient(45deg, #0b5394, #351c75);
-        color: white;
-        font-size: 18px;
-        padding: 10px 20px;
-        border-radius: 10px;
-        transition: 0.3s;
-        box-shadow: 0px 5px 15px rgba(0, 201, 255, 0.4);
-    }
-    .stButton>button:hover {
-        transform: scale(1.05);
-        background: linear-gradient(45deg, #92fe9d, #00c9ff);
-        color: black;
-    }
-    .result-box {
-        font-size: 20px;
-        font-weight: bold;
-        text-align: center;
-        background: rgba(255, 255, 255, 0.1);
-        padding: 15px;
-        color: #00c9ff;
-        border-radius: 10px;
-        margin: 20px;
-        box-shadow: 0px 5px 15px rgba(0, 201, 255, 0.3);
-    }
-    .footer {
-        text-align: center;
-        margin-top: 50px;
-        font-size: 14px;
-        color: black;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+    
 
 #Page configuration
 st.set_page_config(page_title="Scientific Calculator", layout="centered")
